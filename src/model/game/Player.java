a10e1bff50f86ad25862eca88686da91f7191fb1
guillaume <guillaume.laroyenne@tetramind.ai>
2024-02-16 02:33:23 +0100

    player field

--- src/model/game/Player.java
+++ src/model/game/Player.java
@@ -1,27 +1,27 @@
 package model.game;
 
+import model.game.entity.individual.pac.person.JrPacMan;
 import model.game.entity.individual.pac.person.PacPerson;
 
 import java.awt.*;
 
 public final class Player {
-
-    private static final Color DEFAULT_COLOR = Color.YELLOW;
+    public static final Color DEFAULT_COLOR = Color.YELLOW;
 
     private final String name;
     private final Color color;
+    private final Joystick joystick;
+
     private int score;
     private PacPerson avatar;
-
-    private final Joystick joystick;
 
 
     public Player(String name, Color color) {
         this.joystick = new Joystick();
+        avatar = new JrPacMan();
         this.name = name;
         this.color = color;
         score = 0;
-        avatar = null;
     }
 
     public Player(String name) {
@@ -38,5 +38,21 @@ public final class Player {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int value) {
+        score += value;
+    }
+
+    public PacPerson getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(PacPerson avatar) {
+        this.avatar = avatar;
     }
 }
