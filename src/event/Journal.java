21b32db68133658222229a04961888d3dca75294
laroyenneg <laroyenneg@gmail.com>
2024-02-18 21:11:15 +0100

    journal

--- src/event/Journal.java
+++ src/event/Journal.java
@@ -1,18 +1,36 @@
 package event;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Journal {
     private static final Journal INSTANCE = new Journal();
 
-    private final Queue<Event> eventQueue;
+    private final List<Event> events;
 
     private Journal() {
-        eventQueue = new ArrayDeque<>();
+        events = new ArrayList<>();
     }
 
     public Journal getInstance() {
         return INSTANCE;
+    }
+
+    public synchronized void addEvent(Event event) {
+        events.add(event);
+        event.notifyAll();
+    }
+
+    public synchronized Event readEvent(int index) {
+
+        if (events.size() <= index) {
+            try {
+                events.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return (events.size() > index) ? events.get(index) : null;
     }
 }
